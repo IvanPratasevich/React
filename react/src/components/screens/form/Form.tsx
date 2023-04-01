5d71dad0c2f694b184efb82df0493b9d27f5fc95
IvanPratasevich <iv.pratasevich@gmail.com>
2023-04-02 00:20:11 +0300

    refactor: code refactor

--- react/src/components/screens/form/Form.tsx
+++ react/src/components/screens/form/Form.tsx
@@ -2,10 +2,8 @@ import { Form } from '../../ui/form/Form';
 import React from 'react';
 import Main from '../../../structure/main/Main';
 
-class FormPage extends React.Component {
-  render = () => {
-    return <Main components={[Form]} />;
-  };
-}
+const FormPage = () => {
+  return <Main components={[Form]} />;
+};
 
 export default FormPage;
