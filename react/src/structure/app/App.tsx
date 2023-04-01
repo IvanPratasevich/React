19253c21f4aa825a5d1c3561cf250df226002e52
IvanPratasevich <iv.pratasevich@gmail.com>
2023-04-01 13:48:19 +0300

    feat: add hooks to App

--- react/src/structure/app/App.tsx
+++ react/src/structure/app/App.tsx
@@ -3,20 +3,22 @@ import AboutUs from '../../components/screens/about-us/AboutUs';
 import Header from '../header/Header';
 import Home from '../../components/screens/home/Home';
 import React from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { useRoutes } from 'react-router-dom';
 import Form from '../../components/screens/form/Form';
 
 const App = () => {
+  const routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '*', element: <NotFound /> },
+    { path: '/404', element: <NotFound /> },
+    { path: '/about-us', element: <AboutUs /> },
+    { path: '/form', element: <Form /> },
+  ]);
+
   return (
     <>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<Navigate to="/404" />} />
-        <Route path="/404" element={<NotFound />} />
-        <Route path="/about-us" element={<AboutUs />} />
-        <Route path="/form" element={<Form />} />
-      </Routes>
+      {routes}
     </>
   );
 };
